e4c0e076695c70b5e8d7028ff2bc6c05663647c9
Aljoscha Poertner <aljoscha.poertner@hellmann.com>
2024-04-05 14:52:44 +0200

    feat: add vim-rooter plugin
    Signed-off-by: Aljoscha Poertner <aljoscha.poertner@hellmann.com>

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -94,4 +94,5 @@ return {
             { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
         },
   },
+  { "airblade/vim-rooter" }
 }
