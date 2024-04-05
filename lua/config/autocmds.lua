6b5360440954b49a00be04cb38b6e2ec22226768
Aljoscha Poertner <aljoscha.poertner@hellmann.com>
2024-04-05 14:51:21 +0200

    feat: don't open neotree on startup
    Signed-off-by: Aljoscha Poertner <aljoscha.poertner@hellmann.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,9 +3,6 @@ vim.cmd("colorscheme kanagawa-dragon")
 -- --
 
 -- neo-tree --
-vim.api.nvim_create_autocmd("VimEnter", {
-  command = "set nornu nonu | Neotree toggle",
-})
 vim.api.nvim_create_autocmd("BufEnter", {
   command = "set rnu nu",
 })
