b977e07816696d92ec788df071b00d409f0f6ded
Aljoscha Poertner <aljoscha.poertner@hellmann.com>
2024-05-24 14:33:38 +0200

    feat: autocmd for format enable/disable
    Signed-off-by: Aljoscha Poertner <aljoscha.poertner@hellmann.com>

--- lua/config/ide/conform.lua
+++ lua/config/ide/conform.lua
@@ -1,9 +1,11 @@
 require("conform").setup({
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_fallback = true }
+  end,
   formatters_by_ft = {
     lua = { "stylua" },
     -- Conform will run multiple formatters sequentially
